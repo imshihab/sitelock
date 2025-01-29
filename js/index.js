2f79ff468148e81c7b282a180f01ec42efd417ac
Saharia Shihab <imsahariashihab@gmail.com>
2025-01-29 19:45:52 +0600

    add show hide button to dialog

--- js/index.js
+++ js/index.js
@@ -77,6 +77,30 @@ const createPinContainer = () => {
     return container;
 };
 
+const showHidePassword = (dialog) => {
+    // Create the show-hide button
+    const showHideButton = document.createElement("button");
+    showHideButton.type = "button"; // Prevent it from submitting a form
+    showHideButton.className = "show-hide-button";
+    const SHOW__ICON = `<svg viewBox="0 0 24 24" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z"/></svg>`;
+    const HIDE__ICON = `<svg viewBox="0 0 24 24" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M2,5.27L3.28,4L20,20.72L18.73,22L15.65,18.92C14.5,19.3 13.28,19.5 12,19.5C7,19.5 2.73,16.39 1,12C1.69,10.24 2.79,8.69 4.19,7.46L2,5.27M12,9A3,3 0 0,1 15,12C15,12.35 14.94,12.69 14.83,13L11,9.17C11.31,9.06 11.65,9 12,9M12,4.5C17,4.5 21.27,7.61 23,12C22.18,14.08 20.79,15.88 19,17.19L17.58,15.76C18.94,14.82 20.06,13.54 20.82,12C19.17,8.64 15.76,6.5 12,6.5C10.91,6.5 9.84,6.68 8.84,7L7.3,5.47C8.74,4.85 10.33,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C12.69,17.5 13.37,17.43 14,17.29L11.72,15C10.29,14.85 9.15,13.71 9,12.28L5.6,8.87C4.61,9.72 3.78,10.78 3.18,12Z"/></svg>`;
+    showHideButton.innerHTML = SHOW__ICON;
+    const passInput = dialog.querySelector(".passIcon");
+    // Toggle password visibility when the button is clicked
+    showHideButton.addEventListener("click", () => {
+        if (passInput.type === "password") {
+            passInput.type = "text";
+            showHideButton.innerHTML = HIDE__ICON;
+        } else {
+            passInput.type = "password";
+            showHideButton.innerHTML = SHOW__ICON;
+        }
+    });
+
+    const passIconField = dialog.querySelector(".passIconField");
+    passIconField.appendChild(showHideButton);
+};
+
 const createNewDialog = () => {
     const overlay = document.createElement("div");
     overlay.classList.add("overlay-container");
@@ -84,7 +108,7 @@ const createNewDialog = () => {
     const dialog = document.createElement("div");
     dialog.classList.add("dialog-container");
 
-    dialog.innerHTML = `
+    dialog.innerHTML = /*html*/ `
         <h3 class="auth-title">Add New Domain</h3>
         <form id="NewAuthForm">
             <div class="input-container">
@@ -93,7 +117,7 @@ const createNewDialog = () => {
                 <div class="active-indicator"></div>
             </div>
             <p class="supporting-text"></p>
-            <div class="input-container">
+            <div class="input-container passIconField">
                 <input type="password" class="input-field passIcon" id="NewPassword" required placeholder=" ">
                 <label for="NewPassword" class="input-label">Password:</label>
                 <div class="active-indicator"></div>
@@ -104,6 +128,8 @@ const createNewDialog = () => {
             </div>
         </form>
     `;
+
+    showHidePassword(dialog);
 
     overlay.appendChild(dialog);
     return overlay;
@@ -116,10 +142,10 @@ const createDeleteDialog = () => {
     const dialog = document.createElement("div");
     dialog.classList.add("dialog-container");
 
-    dialog.innerHTML = `
+    dialog.innerHTML = /*html*/ `
         <h3 class="auth-title">Authentication Required</h3>
         <form id="authForm">
-            <div class="input-container">
+            <div class="input-container passIconField">
                 <input type="password" class="input-field passIcon" id="password" required placeholder=" ">
                 <label for="password" class="input-label">Password:</label>
                 <div class="active-indicator"></div>
@@ -138,6 +164,7 @@ const createDeleteDialog = () => {
         </form>
     `;
 
+    showHidePassword(dialog);
     overlay.appendChild(dialog);
     return overlay;
 };
@@ -150,7 +177,7 @@ const createPinDialog = () => {
     const dialog = document.createElement("div");
     dialog.classList.add("dialog-container");
 
-    dialog.innerHTML = `
+    dialog.innerHTML = /*html*/ `
         <h3 class="auth-title">Authentication Required</h3>
         <form id="pinForm">
             <div class="pin-input-container">
@@ -284,7 +311,7 @@ async function checkUrl(url) {
 
             return {
                 valid: true,
-                message: response.url,
+                message: new URL(response.url).origin + "/",
             };
         } catch (fetchError) {
             return { valid: false, message: "URL could not be reached" };
@@ -352,6 +379,7 @@ const addDomain = async () => {
                 document.querySelector(".supporting-text").textContent =
                     "Checking site...";
                 const { valid, message } = await checkUrl(site);
+                console.log(valid, message);
 
                 if (valid === false) {
                     toast(message, "error");
@@ -694,7 +722,7 @@ const initApp = async () => {
                     const isFirstInstall = response.isFirstInstall;
                     if (isFirstInstall) {
                         const pinContainer = createPinContainer();
-                        pinContainer.innerHTML = `
+                        pinContainer.innerHTML = /*html*/ `
                         <p>Set a PIN to securely authenticate yourself, even if you forget other passwords. This PIN will also be needed before adding new passkeys.</p>
                         <form id="createPinForm">
                             <h4 class="pintitle">Set a PIN</h4>
@@ -915,7 +943,7 @@ const initApp = async () => {
             const newButton = document.createElement("button");
             newButton.type = "button";
             newButton.className = "newButton ripple_effect";
-            newButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--text-primary)"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+            newButton.innerHTML = /*html*/ `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--text-primary)"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
             <span>New</span>`;
             newButtonContainer.appendChild(newButton);
 
