935fa468dd86383c684d6bc2a1a96208f0bb5973
Saharia Shihab <imsahariashihab@gmail.com>
2025-01-30 12:30:27 +0600

    Init SiteLock

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+    chrome.storage.sync.set({ isFirstInstall: true });
+    chrome.contextMenus.create({
+        id: "Settings",
+        title: "Settings",
+        contexts: ["all"],
+    });
+});
